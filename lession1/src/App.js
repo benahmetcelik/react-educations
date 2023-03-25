cf189a16b4af2b2288a5565cc5b0767696c079f7
webkedi <ben4hmetcelik@gmail.com>
2023-03-25 21:39:27 +0300

    Lession1

---
+++ lession1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+function App() {
+  return (
+    <div className="App">
+      
+      Test24dac
+      <img src={logo} alt=""/> 
+    </div>
+  );
+}
+
+export default App;
